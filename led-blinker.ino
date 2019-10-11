993dc88206a46051562d06f1f78f9d24a1e2f01a
Roland <barkocziroland@gmail.com>
2019-10-11 12:14:48 +0200

    Change in RQ

--- led-blinker.ino
+++ led-blinker.ino
@@ -12,7 +12,7 @@
 
  ***********************************************/
 
-#define LED1 10   //PIN 10 to drive the led
+#define LED1 12   //PIN 12 to drive the led
 #define LED2 11   //PIN 10 to drive the led
 #define DELAY 500 //50ms delay in each loop
 #define MIN_PWM_VALUE 0 //minimum pwm value
